37d661217135e8fdba559f01f4624953a975db5f
João Pedro Seleguim Garcia <136927554+JoaoStormzz@users.noreply.github.com>
2024-11-02 00:55:39 -0300

    Delete tcc-att directory

--- tcc-att/script/tema.js
+++
@@ -1,7 +0,0 @@
-function toggleTheme() {
-    let body = document.body;
-    body.classList.toggle("light-mode");
-    body.classList.toggle("dark-mode");
-}
-
-window.onload = checkTimeAndChangeTheme;
